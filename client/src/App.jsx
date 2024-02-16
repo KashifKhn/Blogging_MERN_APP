673137c027a161776150e14408aa8881eb2f794a
KashifKhn <kashifkhnx04@gmail.com>
2024-02-16 17:06:11 +0500

    feat: pages, navigation template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Home from "./pages/home";
 import CreateNew from "./pages/CreateNew";
 import Edit from "./pages/Edit";
+import SingleBlog from "./pages/SingleBlog";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+    },
+    {
+      path: "/:id",
+      element: <SingleBlog />,
     },
     {
       path: "/new",
---
+++ client/src/pages/SingleBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleBlog = () => {
+  return (
+    <div>SingleBlog</div>
+  )
+}
+
+export default SingleBlog
