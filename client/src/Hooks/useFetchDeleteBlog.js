68853e50af0e06cd6c607d2ae22420c5bc368a5a
KashifKhn <kashifkhnx04@gmail.com>
2024-02-19 05:36:15 +0500

    refacto: blog deletion functionality

---
+++ client/src/Hooks/useFetchDeleteBlog.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useFetchDeleteBlog = (id) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const deleteBlog = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.delete(
+        `${import.meta.env.VITE_API_ENDPOINT}/${id}`
+      );
+      setResponse(res);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    await deleteBlog(id);
+    navigate("/");
+  };
+
+  return { response, error, isLoading, handleDelete };
+};
+
+export default useFetchDeleteBlog;
--- client/src/components/SinglePage/SinglePageCard.jsx
+++ client/src/components/SinglePage/SinglePageCard.jsx
@@ -1,28 +1,12 @@
 import Card from "react-bootstrap/Card";
 import { useParams } from "react-router-dom";
 import useFetchSingleBlog from "../../Hooks/useFetchSingleBlog";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import useFetchDeleteBlog from "../../Hooks/useFetchDeleteBlog";
 
 const SinglePageCard = () => {
-  const navigate = useNavigate();
   const { id } = useParams();
   const { blog } = useFetchSingleBlog(id);
-  console.log(blog);
-
-  const handleDelete = async () => {
-    try {
-      const res = await axios.delete(
-        `${import.meta.env.VITE_API_ENDPOINT}/${id}`
-      );
-      const data = await res.data;
-      console.log(data);
-
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { handleDelete } = useFetchDeleteBlog(id);
 
   return (
     <Card className="w-full border-0">
