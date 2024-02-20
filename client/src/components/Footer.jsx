c2db073655dc3aaea38fa14e9e58285ddfed0cd2
KashifKhn <kashifkhnx04@gmail.com>
2024-02-20 16:46:39 +0500

    style: Adding simple footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="bg-[#f8f9fa] text-black border-t-2">
+      <Container>
+        <div className="flex items-center justify-center py-4">
+          <p>© 2021. All rights reserved.</p>
+        </div>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
