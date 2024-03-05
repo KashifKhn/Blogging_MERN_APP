0301cde8ec09a4bf800af238edec21759c868ca5
KashifKhn <kashifkhnx04@gmail.com>
2024-03-05 09:17:14 +0500

    refactor: Enhance error handling format in errorHandler

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -1,6 +1,12 @@
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message = "Some thing went Wrong!" } = err;
-  res.status(statusCode).json(message);
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
 };
 
 export default errorHandler;
