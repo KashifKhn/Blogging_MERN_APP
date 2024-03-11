9f390b31587bde0d83b53b32df9b57d88883a0a5
KashifKhn <kashifkhnx04@gmail.com>
2024-03-11 18:44:57 +0500

    fix: the succes of error middleware

--- server/src/middlewares/errorHandler.js
+++ server/src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message = "Some thing went Wrong!" } = err;
 
   res.status(statusCode).json({
-    success: false,
+    success: statusCode >= 400 && statusCode < 600 ? false : true,
     statusCode,
     message,
     stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
