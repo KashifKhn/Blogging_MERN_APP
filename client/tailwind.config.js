01c861322bbee9a3c4b96d59639e0c8b49091e7f
KashifKhn <kashifkhnx04@gmail.com>
2024-03-12 15:45:41 +0500

    fix: darkMode form class to selector

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class", '[data-mode="dark"]'],
+  darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
