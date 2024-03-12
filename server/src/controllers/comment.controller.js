b7956e18a303b9cf93902a0758671784f3162c5c
KashifKhn <kashifkhnx04@gmail.com>
2024-03-12 09:18:57 +0500

    refactor: using the Sever Error file in controller

--- server/src/controllers/comment.controller.js
+++ server/src/controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Comment from "../models/comment.model.js";
 import Blog from "../models/blog.model.js";
+import { ServerError } from "../utils/ServerError.js";
 
 const getComments = asyncHandler(async (req, res) => {
   const { blogId } = req.params;
@@ -10,7 +11,7 @@ const getComments = asyncHandler(async (req, res) => {
   if (!comments || comments.length === 0) {
     res.status(404).json({ message: "Blog not found" });
   }
-  res.status(200).json(comments);
+  res.status(200).json(comments.comments);
 });
 
 const createComment = asyncHandler(async (req, res) => {
@@ -18,9 +19,12 @@ const createComment = asyncHandler(async (req, res) => {
   const { comment } = req.body;
   const blog = await Blog.findById(blogId);
   if (!blog) {
-    res.status(404).json({ message: "Blog not found" });
+   throw new ServerError(404, "Blog not found") 
   }
   const newComment = new Comment({ comment });
+  if(!newComment) {
+    throw new ServerError(500, "Comment could not be created")
+  }
   await newComment.save();
   blog.comments.push(newComment);
   await blog.save();
@@ -31,11 +35,11 @@ const deleteComment = asyncHandler(async (req, res) => {
   const { blogId, commentId } = req.params;
   const blog = await Blog.findById(blogId);
   if (!blog) {
-    res.status(404).json({ message: "Blog not found" });
+    throw new ServerError(404, "Blog not found");
   }
   const comment = await Comment.findByIdAndDelete(commentId);
   if (!comment) {
-    res.status(404).json({ message: "Comment not found" });
+    throw new ServerError(404, "Comment not found");
   }
   blog.comments = blog.comments.filter(
     (comment) => comment.toString() !== commentId
