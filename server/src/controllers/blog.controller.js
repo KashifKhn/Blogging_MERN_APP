f4a50868335f148bca7a84eab436dfeaac9672e7
KashifKhn <kashifkhnx04@gmail.com>
2024-03-12 09:14:29 +0500

    refactor: using the Sever Error file in controller

--- server/src/controllers/blog.controller.js
+++ server/src/controllers/blog.controller.js
@@ -1,56 +1,54 @@
-import  Blog  from "../models/blog.model.js";
+import Blog from "../models/blog.model.js";
 import asyncHandler from "express-async-handler";
+import { ServerError } from "../utils/ServerError.js";
 
 const getBlogs = asyncHandler(async (req, res) => {
-  try {
-    const blogs = await Blog.find();
-    res.status(200).json(blogs);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const blogs = await Blog.find();
+  if (!blogs || blogs.length === 0) {
+    throw new ServerError(404, "No blogs found in the database.");
   }
+  res.status(200).json(blogs);
 });
 
 const getBlog = asyncHandler(async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    res.status(200).json(blog);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    throw new ServerError(404, "Blog not found. Please check the id.");
   }
+  res.status(200).json(blog);
 });
 
 const createBlog = asyncHandler(async (req, res) => {
   const blog = new Blog(req.body);
-
-  try {
-    const newBlog = await blog.save();
-    res.status(201).json(newBlog);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+  const newBlog = await blog.save();
+  if (!newBlog) {
+    throw new ServerError(500, "Blog could not be created.");
   }
+  res.status(201).json(newBlog);
 });
 
 const updateBlog = asyncHandler(async (req, res) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-    blog.title = req.body.title;
-    blog.imgUrl = req.body.imgUrl;
-    blog.post = req.body.post;
-    blog.postFormat = req.body.postFormat;
-    const updatedBlog = await blog.save();
-    res.status(200).json(updatedBlog);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const blog = await Blog.findById(req.params.id);
+  if (!blog) {
+    throw new ServerError(404, "Blog not found. Please check the id.");
   }
+  blog.title = req.body.title;
+  blog.imgUrl = req.body.imgUrl;
+  blog.post = req.body.post;
+  blog.postFormat = req.body.postFormat;
+  const updatedBlog = await blog.save();
+  if (!updatedBlog) {
+    throw new ServerError(500, "Blog could not be updated.");
+  }
+  res.status(200).json(updatedBlog);
 });
 
 const deleteBlog = asyncHandler(async (req, res) => {
-  try {
-    await Blog.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: "Blog deleted" });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
+  if (!deletedBlog) {
+    throw new ServerError(404, "Blog not found. Please check the id.");
   }
+  res.status(200).json({ message: "Blog deleted" });
 });
 
 export { getBlogs, getBlog, createBlog, updateBlog, deleteBlog };
