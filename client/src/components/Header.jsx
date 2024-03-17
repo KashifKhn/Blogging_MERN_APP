e962398fb525491a9452fb18e2ca6fe3845100ab
KashifKhn <kashifkhnx04@gmail.com>
2024-03-17 12:31:35 +0500

    refactor: Update header UI to use Tailwind CSS and add logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,112 +1,116 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/svgs/Logo";
 
 const Header = () => {
   return (
-    <Navbar
-      sticky="top"
-      expand="md"
-      className="bg-body-tertiary border-b-2"
-    >
-      <div className="container">
-        <Navbar.Brand className="mr-16">
+    <header className="antialiased border-b-2 dark:bg-gray-900 border-gray-200">
+      <nav className="flex container flex-wrap gap-4 px-4 lg:px-6 py-2.5 justify-center sm:justify-between items-center">
+        <div className="flex justify-start items-center">
           <NavLink
             to="/"
-            className="text-3xl font-bold text-red-500"
+            className="flex mr-8"
           >
-            Blog App
+            <Logo />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap sr-only dark:text-white">
+              EnvisionEssence
+            </span>
           </NavLink>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <div>
-            <form className="w-full">
-              <label
-                htmlFor="default-search"
-                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-              >
-                Search
-              </label>
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  className="block w-full outline-none px-8 py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search"
-                  required=""
-                />
-              </div>
-            </form>
-          </div>
-
-          <Nav
-            className="ml-auto my-2 my-lg-0"
-            style={{ maxHeight: "300px" }}
-            navbarScroll
+          <form
+            action="#"
+            method="GET"
+            className="lg:pl-2 "
           >
-            <div className="nav-link flex items-center justify-center mr-4">
-              <NavLink
-                to="/new"
-                className={
-                  "flex gap-2 opacity-60 hover:opacity-100 transition-all duration-400 ease-in-out"
-                }
-              >
+            <label
+              htmlFor="topbar-search"
+              className="sr-only"
+            >
+              Search
+            </label>
+            <div className="relative mt-1 lg:w-96">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                 <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  aria-label="Write"
+                  viewBox="0 0 20 20"
                 >
+                  {" "}
                   <path
-                    d="M14 4a.5.5 0 0 0 0-1v1zm7 6a.5.5 0 0 0-1 0h1zm-7-7H4v1h10V3zM3 4v16h1V4H3zm1 17h16v-1H4v1zm17-1V10h-1v10h1zm-1 1a1 1 0 0 0 1-1h-1v1zM3 20a1 1 0 0 0 1 1v-1H3zM4 3a1 1 0 0 0-1 1h1V3z"
-                    fill="currentColor"
-                  ></path>
-                  <path
-                    d="M17.5 4.5l-8.46 8.46a.25.25 0 0 0-.06.1l-.82 2.47c-.07.2.12.38.31.31l2.47-.82a.25.25 0 0 0 .1-.06L19.5 6.5m-2-2l2.32-2.32c.1-.1.26-.1.36 0l1.64 1.64c.1.1.1.26 0 .36L19.5 6.5m-2-2l2 2"
                     stroke="currentColor"
-                  ></path>
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />{" "}
                 </svg>
-                <span>write</span>
-              </NavLink>
+              </div>
+              <input
+                type="text"
+                id="topbar-search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-full focus:ring-primary-500 focus:border-primary-500 block w-full pl-9 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Search"
+              />
             </div>
-            <div className="w-8 h-8 grid place-content-center ml-3 border-[1px] border-gray-300 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-              <svg
-                className="w-10 h-10 text-gray-400"
+          </form>
+        </div>
+        <div className="flex items-center order-2">
+          <NavLink
+            to="/new"
+            className="flex p-2 mr-4 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              aria-label="Write"
+            >
+              <path
+                d="M14 4a.5.5 0 0 0 0-1v1zm7 6a.5.5 0 0 0-1 0h1zm-7-7H4v1h10V3zM3 4v16h1V4H3zm1 17h16v-1H4v1zm17-1V10h-1v10h1zm-1 1a1 1 0 0 0 1-1h-1v1zM3 20a1 1 0 0 0 1 1v-1H3zM4 3a1 1 0 0 0-1 1h1V3z"
                 fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+              ></path>
+              <path
+                d="M17.5 4.5l-8.46 8.46a.25.25 0 0 0-.06.1l-.82 2.47c-.07.2.12.38.31.31l2.47-.82a.25.25 0 0 0 .1-.06L19.5 6.5m-2-2l2.32-2.32c.1-.1.26-.1.36 0l1.64 1.64c.1.1.1.26 0 .36L19.5 6.5m-2-2l2 2"
+                stroke="currentColor"
+              ></path>
+            </svg>
+            <span>write</span>
+          </NavLink>
+          <button
+            type="button"
+            data-dropdown-toggle="notification-dropdown"
+            className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          >
+            <span className="sr-only">View notifications</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 14 20"
+            >
+              <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            id="user-menu-button"
+            aria-expanded="false"
+            data-dropdown-toggle="dropdown"
+          >
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              alt="user photo"
+            />
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
