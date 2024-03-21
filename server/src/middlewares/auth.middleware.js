be0339266c06ee1effb690404887fc9c40e93584
KashifKhn <kashifkhnx04@gmail.com>
2024-03-21 20:05:58 +0500

    fix: Refactor auth.middleware.js for improved token handling

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,27 +4,27 @@ import { User } from "../models/User.model.js";
 import { ServerError } from "../utils/ServerError.js";
 
 const verifyJwt = asyncHandler(async (req, res, next) => {
- 
   const token =
     (req.headers?.authorization && req.headers.authorization.split(" ")[1]) ||
-    req.cookies.accessToken;
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+    (req.headers?.Authorization && req.headers.Authorization.split(" ")[1]) ||
+    req.cookies?.accessToken;
 
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decoded._id);
-
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
+  if (!token) {
     throw new ServerError(401, "Unauthorized");
   }
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  if (!decoded) {
+    throw new ServerError(401, "Unauthorized");
+  }
+
+  const user = await User.findById(decoded._id).select("-password");
+
+  if (!user) {
+    throw new ServerError(401, "Unauthorized");
+  }
+  req.user = user;
+  next();
 });
 
 export { verifyJwt };
