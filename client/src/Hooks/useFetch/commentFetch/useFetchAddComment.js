8881d5ec137fc10f86398fdb2403b41f62eea09c
KashifKhn <kashifkhnx04@gmail.com>
2024-03-26 16:07:44 +0500

    refactor: useFetchAddComment hook to use useFetch

--- client/src/Hooks/useFetch/commentFetch/useFetchAddComment.js
+++ client/src/Hooks/useFetch/commentFetch/useFetchAddComment.js
@@ -1,37 +1,18 @@
-import { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import useFetch from "../useFetch";
 
 const useFetchAddComment = (data, blogId) => {
-  const navigate = useNavigate();
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { response, error, isLoading, fetchData } = useFetch();
 
   const createNewBlog = async () => {
-    setIsLoading(true);
-
-    const abortCont = new AbortController();
-    try {
-      const res = await axios.post(
-        `${import.meta.env.VITE_API_ENDPOINT}/${blogId}/comments`,
-        data,
-        {
-          signal: abortCont.signal,
-        }
-      );
-      setResponse(res);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
+    fetchData(`${blogId}/comments`, {
+      method: "POST",
+      data: data,
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await createNewBlog();
-    navigate(`/${blogId}`);
   };
   return { response, error, isLoading, handleSubmit };
 };
