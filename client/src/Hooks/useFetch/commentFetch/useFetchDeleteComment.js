aca0ff4f5a520e16c5120ed5887dc45243dba4d8
KashifKhn <kashifkhnx04@gmail.com>
2024-03-26 16:19:08 +0500

    refactor: useFetchDeleteComment hook to use useFetch

--- client/src/Hooks/useFetch/commentFetch/useFetchDeleteComment.js
+++ client/src/Hooks/useFetch/commentFetch/useFetchDeleteComment.js
@@ -1,27 +1,12 @@
-import { useState } from "react";
-import axios from "axios";
+import useFetch from "../useFetch";
 
 const useFetchDeleteComment = () => {
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { response, error, isLoading, fetchData } = useFetch();
 
   const deleteComment = async (blogId, commentId) => {
-    setIsLoading(true);
-    const abortCont = new AbortController();
-    try {
-      const res = await axios.delete(
-        `${import.meta.env.VITE_API_ENDPOINT}/${blogId}/comments/${commentId}`,
-        {
-          signal: abortCont.signal,
-        }
-      );
-      setResponse(res);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
+    fetchData(`${blogId}/comments/${commentId}`, {
+      method: "DELETE",
+    });
   };
 
   const handleDelete = async (blogId, commentId) => {
