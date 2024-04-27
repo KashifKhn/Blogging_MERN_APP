57f658030addd1f9ebf9397fa3b313485726beb1
KashifKhn <kashifkhnx04@gmail.com>
2024-04-27 13:13:06 +0500

    refactor: NavBar component to conditionally render AuthButton, user profile and notification button based on authentication state

--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -5,8 +5,10 @@ import SearchBar from "./SearchBar";
 import ChangeTheme from "./ChangeTheme";
 import UserSettingIcon from "./UserSettingIcon";
 import AuthButton from "./AuthButton";
+import useAuth from "../../Hooks/auth/useAuth";
 
 const NavBar = () => {
+  const { authState } = useAuth();
   return (
     <nav className="flex container flex-wrap gap-4 px-4 lg:px-6 py-2.5 justify-center sm:justify-between items-center">
       <div className="flex justify-start items-center">
@@ -39,22 +41,26 @@ const NavBar = () => {
           </svg>
           <span>write</span>
         </NavLink>
-        <AuthButton />
-        <button
-          type="button"
-          data-dropdown-toggle="notification-dropdown"
-          className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
-          <span className="sr-only">View notifications</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 14 20">
-            <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
-          </svg>
-        </button>
-        <UserSettingIcon />
+        {!authState.isAuthenticated && <AuthButton />}
+        {authState.isAuthenticated && (
+          <>
+            <button
+              type="button"
+              data-dropdown-toggle="notification-dropdown"
+              className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+              <span className="sr-only">View notifications</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 14 20">
+                <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+              </svg>
+            </button>
+            <UserSettingIcon />
+          </>
+        )}
         <ChangeTheme />
       </div>
     </nav>
