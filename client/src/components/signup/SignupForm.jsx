239ab630a1621bb33526cf786d10aff63a90cdcb
KashifKhn <kashifkhnx04@gmail.com>
2024-04-28 17:17:06 +0500

    fix:  SignupForm submit button styling in

--- client/src/components/signup/SignupForm.jsx
+++ client/src/components/signup/SignupForm.jsx
@@ -244,7 +244,7 @@ const SignupForm = () => {
       </div>
       <button
         type="submit"
-        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        className="w-full flex items-center justify-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         disabled={isLoading}>
         {isLoading ? (
           <div role="status">
@@ -268,7 +268,6 @@ const SignupForm = () => {
         ) : (
           "Create an account"
         )}
-        Create an account
       </button>
       <p className="text-sm font-light text-gray-500 dark:text-gray-400">
         Already have an account?{" "}
