c7e1ba12feeff02de0ffe1d99cc09513fec7a9f4
KashifKhn <kashifkhnx04@gmail.com>
2024-04-30 16:59:51 +0500

    refactor: CommentCard component to include dynamic author information

--- client/src/components/comments/CommentCard.jsx
+++ client/src/components/comments/CommentCard.jsx
@@ -15,24 +15,22 @@ const CommentCard = ({ comment, handleDelete }) => {
   return (
     <article
       onClick={handleDropdown}
-      className="p-6 text-base border-b border-gray-300 dark:border-gray-700 rounded-lg dark:bg-gray-900"
-    >
+      className="p-6 text-base border-b border-gray-300 dark:border-gray-700 rounded-lg dark:bg-gray-900">
       <div className="flex justify-between items-center mb-2 relative">
         <div className="flex items-center">
           <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
             <img
               className="mr-2 w-6 h-6 rounded-full"
               src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-              alt="Michael Gough"
+              alt={`${comment?.author?.fullname} profile picture`}
             />
-            Michael Gough
+            {comment?.author?.fullname}
           </p>
           <p className="text-sm text-gray-600 dark:text-gray-400">
             <time
               pubdate=""
               dateTime="2022-02-08"
-              title="February 8th, 2022"
-            >
+              title="February 8th, 2022">
               {getTimeAgo(comment.createdAt)}
             </time>
           </p>
@@ -43,15 +41,13 @@ const CommentCard = ({ comment, handleDelete }) => {
             data-dropdown-toggle="dropdownComment1"
             className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             type="button"
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-          >
+            onClick={() => setDropdownOpen(!dropdownOpen)}>
             <svg
               className="w-4 h-4"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
-              viewBox="0 0 16 3"
-            >
+              viewBox="0 0 16 3">
               <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
             </svg>
             <span className="sr-only">Comment settings</span>
@@ -61,12 +57,10 @@ const CommentCard = ({ comment, handleDelete }) => {
             className={`z-10 w-36 ${
               !dropdownOpen && "hidden"
             } absolute right-4 top-8 rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600`}
-            data-popper-placement="bottom"
-          >
+            data-popper-placement="bottom">
             <ul
               className="py-1 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdownMenuIconHorizontalButton"
-            >
+              aria-labelledby="dropdownMenuIconHorizontalButton">
               <li>
                 <button className="block w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                   Edit
@@ -75,8 +69,7 @@ const CommentCard = ({ comment, handleDelete }) => {
               <li>
                 <button
                   className="block w-full py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  onClick={() => handleDelete(blogId, comment._id)}
-                >
+                  onClick={() => handleDelete(blogId, comment._id)}>
                   Remove
                 </button>
               </li>
@@ -93,15 +86,13 @@ const CommentCard = ({ comment, handleDelete }) => {
       <div className="flex items-center mt-4 space-x-4">
         <button
           type="button"
-          className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
-        >
+          className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
           <svg
             className="mr-1.5 w-3.5 h-3.5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 20 18"
-          >
+            viewBox="0 0 20 18">
             <path
               stroke="currentColor"
               strokeLinecap="round"
