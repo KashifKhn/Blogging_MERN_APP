77ac76943209f5366f3031f291091bd72b73d7f7
KashifKhn <kashifkhnx04@gmail.com>
2024-04-30 15:47:10 +0500

    fix: isAuthorBlog middleware to throw ServerError with specific error messages

--- server/src/middlewares/isAuthorBlog.middleware.js
+++ server/src/middlewares/isAuthorBlog.middleware.js
@@ -3,15 +3,16 @@ import { Blog } from "../models/blog.model.js";
 import { ServerError } from "../utils/ServerError.js";
 
 const isAuthorBlog = asyncHandler(async (req, res, next) => {
-  const blogId = req.params.id;
+  const blogId = req?.params?.id;
   const userId = req?.user?._id;
 
   const blog = await Blog.findById(blogId);
+
   if (!blog) {
-    return res.status(404).json({ message: "Blog not found" });
+    throw new ServerError(404, "Blog not found. Please check the id.");
   }
 
-  if (blog.author.toString() !== userId.toString()) {
+  if (!blog.author.equals(userId)) {
     throw new ServerError(403, "You are not the author of this blog");
   }
 
--- server/src/routers/blog.router.js
+++ server/src/routers/blog.router.js
@@ -18,9 +18,7 @@ router.use(verifyJwt);
 router.get("/:id", getBlog);
 router.post("/", createBlog);
 
-router.use(isAuthorBlog);
-
-router.put("/edit/:id", updateBlog);
-router.delete("/:id", deleteBlog);
+router.put("/edit/:id", isAuthorBlog, updateBlog);
+router.delete("/:id", isAuthorBlog, deleteBlog);
 
 export default router;
