2c0f6ef63122cf77b295dd548791e3bf0b04c2cd
KashifKhn <kashifkhnx04@gmail.com>
2024-04-30 16:35:04 +0500

    refactor: Add author field to commentSchema in comment.model.js

--- server/src/models/comment.model.js
+++ server/src/models/comment.model.js
@@ -6,6 +6,10 @@ const commentSchema = new Schema(
       type: String,
       required: true,
     },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
     likes: [
       {
         type: Schema.Types.ObjectId,
