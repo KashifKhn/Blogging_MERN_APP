2bda84ab2b6bcd08a3d07e49b4035eacc844466c
KashifKhn <kashifkhnx04@gmail.com>
2024-04-30 14:37:40 +0500

    feat: Add isAuthorBlog middleware for blog authorization

--- server/src/routers/blog.router.js
+++ server/src/routers/blog.router.js
@@ -7,13 +7,19 @@ import {
   updateBlog,
 } from "../controllers/blog.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { isAuthorBlog } from "../middlewares/isAuthorBlog.middleware.js";
 
 const router = Router();
 
 router.get("/", getBlogs);
+
 router.use(verifyJwt);
+
 router.get("/:id", getBlog);
 router.post("/", createBlog);
+
+router.use(isAuthorBlog);
+
 router.put("/edit/:id", updateBlog);
 router.delete("/:id", deleteBlog);
 
