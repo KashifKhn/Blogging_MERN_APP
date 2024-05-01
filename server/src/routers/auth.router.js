f201cccb63790d84827db6d8c13d88a46c269ab4
KashifKhn <kashifkhnx04@gmail.com>
2024-05-01 10:36:24 +0500

    fix auth.router.js to remove unused middleware

--- server/src/routers/auth.router.js
+++ server/src/routers/auth.router.js
@@ -5,13 +5,12 @@ import {
   logout,
   refresh,
 } from "../controllers/auth.controller.js";
-import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/refresh", refresh);
-router.post("/logout", verifyJwt, logout);
+router.post("/logout", logout);
 
 export default router;
