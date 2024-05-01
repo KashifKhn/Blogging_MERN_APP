6efbe682fb46bafc3d027d8e9272e364d5ef91f9
KashifKhn <kashifkhnx04@gmail.com>
2024-05-01 09:14:25 +0500

    fix: useRefreshToken to handle "Refresh token not found" error or refresh token expire error

--- client/src/Hooks/auth/useRefreshToken.js
+++ client/src/Hooks/auth/useRefreshToken.js
@@ -1,18 +1,26 @@
 import useAuth from "./useAuth";
 import { axiosPublic } from "../../api/axiosInstance";
+import useLogout from "./useLogout";
 
 const useRefreshToken = () => {
   const { setAuthState } = useAuth();
+  const { logout } = useLogout();
 
   const refresh = async () => {
+    try {
       const response = await axiosPublic.get("auth/refresh", {
         withCredentials: true,
       });
-      
+
       setAuthState((prev) => {
         return { ...prev, accessToken: response.data.user.accessToken };
       });
       return response.data.user.accessToken;
+    } catch (error) {
+      if (error?.response?.data?.message === "Refresh token not found") {
+        logout();
+      }
+    }
   };
   return refresh;
 };
