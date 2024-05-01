fb0f88b9a903cb162a05af625dbfaae9182a51b4
KashifKhn <kashifkhnx04@gmail.com>
2024-05-01 11:14:48 +0500

    refactor: useLogout hook to handle logout functionality and make API call to logout endpoint

--- client/src/Hooks/auth/useLogout.js
+++ client/src/Hooks/auth/useLogout.js
@@ -2,7 +2,16 @@ import { useContext } from "react";
 import AuthContext from "../../context/auth/AuthContext";
 
 const useLogout = () => {
-  const { logout } = useContext(AuthContext);
+  const { logout: authLogout } = useContext(AuthContext);
+
+  const logout = async () => {
+    try {
+      await axiosPrivate.post("/auth/logout");
+      authLogout();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return { logout };
 };
