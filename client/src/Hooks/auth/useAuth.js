40145655fd766b22280231829bff418ef86c8220
KashifKhn <kashifkhnx04@gmail.com>
2024-05-02 15:07:48 +0500

    refactor: useAuth hook to include persistent login functionality

--- client/src/Hooks/auth/useAuth.js
+++ client/src/Hooks/auth/useAuth.js
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import AuthContext from "../../context/auth/AuthContext";
 
 const useAuth = () => {
-  const { authState, setAuthState } = useContext(AuthContext);
+  const { authState, setAuthState, persistent, setPersistent } =
+    useContext(AuthContext);
 
-  return { authState, setAuthState };
+  return { authState, setAuthState, persistent, setPersistent };
 };
 
 export default useAuth;
