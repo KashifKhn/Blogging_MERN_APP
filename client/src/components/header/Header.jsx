3b8f82e3d9e89f6f42732299d3ae735ed3e4f6a9
KashifKhn <kashifkhnx04@gmail.com>
2024-05-03 18:48:41 +0500

    fix:  persistent remember bug that not to login

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import NavBar from "./NavBar";
+import useAuth from "../../Hooks/auth/useAuth";
+import useLogout from "../../Hooks/auth/useLogout";
 
 const Header = () => {
+  const { authState, persistent, setPersistent } = useAuth();
+  const { logout } = useLogout();
+
+  useEffect(() => {
+    const handleAfterLoad = (event) => {
+      if(!authState.isAuthenticated) {
+        setPersistent(false);
+      }
+      if (!persistent) {
+        console.log("logging out");
+        logout();
+        setPersistent(false);
+      }
+    };
+
+    window.addEventListener("load", handleAfterLoad);
+
+    return () => {
+      window.removeEventListener("load", handleAfterLoad);
+    };
+  }, []);
+
   return (
     <header className="antialiased border-b-2 dark:bg-gray-900 border-gray-200 dark:border-gray-700">
       <NavBar />
