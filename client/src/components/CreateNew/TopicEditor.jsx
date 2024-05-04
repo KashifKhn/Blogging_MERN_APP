55854992aad1e8d71aaf9e3856106d647f8c8c9c
KashifKhn <kashifkhnx04@gmail.com>
2024-05-04 10:55:43 +0500

    feat: Add TopicEditor component for managing tags in CreateNew page

---
+++ client/src/components/CreateNew/TopicEditor.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { WithContext as ReactTags } from "react-tag-input";
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+const TopicEditor = () => {
+  const [tags, setTags] = useState([]);
+
+  const handleDelete = (i) => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
+  const handleAddition = (tag) => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+    setTags(newTags);
+  };
+
+  const handleTagClick = (index) => {
+    console.log("The tag at index " + index + " was clicked");
+  };
+  return (
+    <div>
+      {" "}
+      <ReactTags
+        tags={tags}
+        // suggestions={suggestions}
+        delimiters={delimiters}
+        handleDelete={handleDelete}
+        handleAddition={handleAddition}
+        handleDrag={handleDrag}
+        handleTagClick={handleTagClick}
+        inputFieldPosition="bottom"
+        autocomplete
+      />
+    </div>
+  );
+};
+
+export default TopicEditor;
