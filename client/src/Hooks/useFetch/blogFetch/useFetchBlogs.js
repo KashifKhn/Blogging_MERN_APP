365f0e2b72dae30ed4af662b14cf43af9c411c35
KashifKhn <kashifkhnx04@gmail.com>
2024-05-05 15:13:19 +0500

    refactor: Update useFetchBlogs hook to include pagination support

--- client/src/Hooks/useFetch/blogFetch/useFetchBlogs.js
+++ client/src/Hooks/useFetch/blogFetch/useFetchBlogs.js
@@ -1,19 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFetch from "../useFetch";
 
 const useFetchBlogs = () => {
+  const [hasMore, setHasMore] = useState(false);
   const { response, isLoading, error, fetchData, abortFetch } = useFetch();
-  const blogs = response?.data;
+  const blogs = response?.data?.results;
 
-  const fetchBlogs = async () => {
-    await fetchData(`blogs`);
+  useEffect(() => {
+    if (response?.data?.next) {
+      setHasMore(true);
+    } else {
+      setHasMore(false);
+    }
+  }, [response]);
+
+  const fetchBlogs = async (page, limit) => {
+    if (page && limit) {
+      await fetchData(`blogs?page=${page}&limit=${limit}`);
+      return;
+    }
+    await fetchData("blogs");
   };
 
   useEffect(() => {
-    fetchBlogs();
+    fetchBlogs(1, 5);
   }, []);
 
-  return { blogs, isLoading, error };
+  return { blogs, isLoading, error, fetchBlogs, hasMore };
 };
 
 export default useFetchBlogs;
