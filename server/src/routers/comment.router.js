2ce2be93058d46cdb6323c40d749ae774f62d233
KashifKhn <kashifkhnx04@gmail.com>
2024-05-05 10:47:27 +0500

    refactor: Update comment router to include pagination middleware for getting all comments

--- server/src/routers/comment.router.js
+++ server/src/routers/comment.router.js
@@ -6,12 +6,17 @@ import {
 } from "../controllers/comment.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { isAuthorComment } from "../middlewares/isAuthorComment.middleware.js";
+import { pagination } from "../middlewares/pagination.middleware.js";
+import { Comment } from "../models/comment.model.js";
 
 const router = express.Router({
   mergeParams: true,
 });
 
-router.route("/").get(getComments).post(verifyJwt, createComment);
+router
+  .route("/")
+  .get(pagination(Comment), getComments)
+  .post(verifyJwt, createComment);
 router.delete("/:commentId", verifyJwt, isAuthorComment, deleteComment);
 
 export default router;
