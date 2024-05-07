68d670e2866a240bf91624984e6af55015bc63a5
KashifKhn <kashifkhnx04@gmail.com>
2024-05-07 15:19:10 +0500

    feat: implement infinite scrolling or  pagination support and improve blog fetching logic

--- client/src/Hooks/useFetch/blogFetch/useFetchBlogs.js
+++ client/src/Hooks/useFetch/blogFetch/useFetchBlogs.js
@@ -4,13 +4,20 @@ import useFetch from "../useFetch";
 const useFetchBlogs = () => {
   const [hasMore, setHasMore] = useState(false);
   const { response, isLoading, error, fetchData, abortFetch } = useFetch();
-  const blogs = response?.data?.results;
+  const [blogs, setBlogs] = useState([]);
+  // const blogs = response?.data?.results;
 
   useEffect(() => {
     if (response?.data?.next) {
       setHasMore(true);
     } else {
       setHasMore(false);
+    }
+
+    if (response?.data?.previous) {
+      setBlogs((prevBlogs) => [...prevBlogs, ...response.data.results]);
+    } else {
+      setBlogs(response?.data?.results);
     }
   }, [response]);
 
@@ -22,9 +29,9 @@ const useFetchBlogs = () => {
     await fetchData("blogs");
   };
 
-  useEffect(() => {
-    fetchBlogs(1, 5);
-  }, []);
+  // useEffect(() => {
+  //   fetchBlogs(1, 5);
+  // }, []);
 
   return { blogs, isLoading, error, fetchBlogs, hasMore };
 };
--- client/src/components/HomePage/BlogCardList.jsx
+++ client/src/components/HomePage/BlogCardList.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import BlogCard from "./BlogCard";
 import useFetchBlogs from "../../Hooks/useFetch/blogFetch/useFetchBlogs";
 import LoadingSkeleton from "./LoadingSkeleton";
 
 const BlogCardList = () => {
-  const { blogs, isLoading, error } = useFetchBlogs();
+  const { blogs, isLoading, error, hasMore, fetchBlogs } = useFetchBlogs();
+  const [page, setPage] = useState(1);
+
+  const observer = useRef();
+  const lastBlogElementRef = useCallback(
+    (node) => {
+      if (isLoading) {
+        return;
+      }
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isLoading, hasMore]
+  );
+
+  useEffect(() => {
+    fetchBlogs(page, 10);
+  }, [page]);
 
   if (isLoading || !blogs) {
     return (
@@ -23,12 +47,24 @@ const BlogCardList = () => {
     <section className="dark:bg-gray-900">
       <div className="py-4 px-1 mx-auto max-w-screen-xl lg:py-8 lg:px-6">
         <div className="grid gap-4">
-          {blogs.map((blog) => (
-            <BlogCard
-              key={blog._id}
-              blog={blog}
-            />
-          ))}
+          {blogs.map((blog, index) => {
+            if (blogs.length === index + 1) {
+              return (
+                <BlogCard
+                  key={blog._id}
+                  blog={blog}
+                  ref={lastBlogElementRef}
+                />
+              );
+            } else {
+              return (
+                <BlogCard
+                  key={blog._id}
+                  blog={blog}
+                />
+              );
+            }
+          })}
         </div>
       </div>
     </section>
