3d6cb21734893d89cf9968b54d696033a621dec6
KashifKhn <kashifkhnx04@gmail.com>
2024-05-30 16:28:15 +0500

    feat: Add user profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import PersistentLogin from "./components/auth/PersistentLogin";
+import UserProfile from "./pages/UserProfile";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -62,6 +63,15 @@ const App = () => {
             </ProtectedRoute>
           ),
         },
+        {
+          path: "user/:id",
+          element: (
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          ),
+        },
+
         {
           path: "*",
           element: <Error />,
