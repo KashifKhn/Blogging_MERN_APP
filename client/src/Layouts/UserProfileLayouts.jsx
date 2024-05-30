435dcee7ba0bbec26d313d0a34f52410e7ff69c8
KashifKhn <kashifkhnx04@gmail.com>
2024-05-30 17:31:21 +0500

    feat: Add Outlet component to UserProfileLayouts

--- client/src/Layouts/UserProfileLayouts.jsx
+++ client/src/Layouts/UserProfileLayouts.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/UserProfile/Header";
+import { Outlet } from "react-router-dom";
 
 const UserProfileLayouts = () => {
   return (
     <div>
       <Header />
+      <Outlet />
     </div>
   );
 };
